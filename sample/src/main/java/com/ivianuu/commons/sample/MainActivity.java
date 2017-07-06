9e2809c02cc0ae9a46ff9151a146a8070c9bc2f8
IVIanuu <IVIanuu@gmail.com>
2017-07-06 14:41:17 +0200

    Changed gradle

--- sample/src/main/java/com/ivianuu/commons/sample/MainActivity.java
+++ sample/src/main/java/com/ivianuu/commons/sample/MainActivity.java
@@ -3,12 +3,14 @@ package com.ivianuu.commons.sample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.ivianuu.commons.Commons;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        Commons.init(getApplicationContext());
 
     }
 }
